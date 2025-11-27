5d4340f6296dfc0e9dac1d9084d370c4bdc5cbe8
BVTRay <137275813+BVTRay@users.noreply.github.com>
2025-11-27 20:14:38 +0800

    自行优化electron

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,13 @@
-import { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
-  appId: 'com.benxi.alumni',
-  appName: '本溪市高级中学校友汇',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
-};
+// import { CapacitorConfig } from '@capacitor/cli';
 
-export default config;
+// const config: CapacitorConfig = {
+//   appId: 'com.benxi.alumni',
+//   appName: '本溪市高级中学校友汇',
+//   webDir: 'dist',
+//   server: {
+//     androidScheme: 'https'
+//   }
+// };
+
+// export default config;
